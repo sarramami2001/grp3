098fcca622131ac5da99459b9b814987480c0399
chyraz <chyrazhanzouty@gmail.com>
2024-01-21 00:12:49 +0000

    Add Annotations to the Class

--- src/main/java/com/example/demo/controller/CrudController.java
+++ src/main/java/com/example/demo/controller/CrudController.java
@@ -11,8 +11,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;//1st modification
 import com.example.demo.service.ICrudService;
 
+@RestController //1st modification
 public class CrudController<T, ID> {
 
 	@Autowired
